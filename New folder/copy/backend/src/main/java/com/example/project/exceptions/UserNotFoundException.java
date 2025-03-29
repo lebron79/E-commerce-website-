501355eb867c46c47d244dbad4310decbec39d51
Yassine <yassinenemri0@gmail.com>
2025-03-29 03:39:19 +0100

    Delete New folder directory

--- New folder/copy/backend/src/main/java/com/example/project/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-// UserNotFoundException.java
-package com.example.project.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
